1022a36f573388759fef405a4beb69cb05df43b3
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-05-18 17:25:52 +1200

    Pass null function from ListView

--- packages/invoices/src/OutboundShipment/ListView/ListView.tsx
+++ packages/invoices/src/OutboundShipment/ListView/ListView.tsx
@@ -80,7 +80,7 @@ export const OutboundShipmentListViewComponent: FC = () => {
       ],
       'selection',
     ],
-    { onChangeSortBy: updateSortQuery, sortBy },
+    { onChangeSortBy: () => {}, sortBy },
     [sortBy]
   );
 
